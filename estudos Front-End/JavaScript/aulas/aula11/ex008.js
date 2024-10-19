4213bcdf900fef601ae033d1205c3ce7601754c7
Antonio Felix de Oliveira Neto <antoniofelixxx7@gmail.com>
2024-10-19 17:34:23 -0300

    organizando pastas.

--- estudos Front-End/JavaScript/aulas/aula11/ex008.js
+++
@@ -1,9 +0,0 @@
-var vel = 60.5
-var max = 60.0
-
-if(vel > max){
-    console.log(`A velocidade do seu carro é de ${vel}km/h`)
-    console.log(`Você está acima da velocidade média permitida, que é ${max}km/h .`)
-    console.log('Você foi mutado.')
-} // condição simples.
-console.log('Diriga com o cinto de segurança')
--- estudos Front-End/JavaScript/aulas/aula15/ambiente.js
+++
@@ -1,9 +0,0 @@
-let num = [5, 2, 3, 4, 5]
-console.log(num)
-num.push(1)
-
-    for(let pos in num){
-        console.log(`A posição ${pos} tem o valor ${num[pos]}`)
-    }
-
-
