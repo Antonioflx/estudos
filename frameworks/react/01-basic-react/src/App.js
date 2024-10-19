435b1729d99e38c392656b72625d02ed42cd82e5
Antonio Felix de Oliveira Neto <antoniofelixxx7@gmail.com>
2024-10-19 17:51:22 -0300

    React

---
+++ frameworks/react/01-basic-react/src/App.js
@@ -0,0 +1,13 @@
+
+import Hello from "./Hello";
+
+function App() {
+  return (
+    <>
+      <h1> Hello World! </h1>
+      <Hello />
+    </>
+  );
+}
+
+export default App;
---
+++ frameworks/react/01-basic-react/src/Hello.jsx
@@ -0,0 +1,10 @@
+function Hello () {
+    return (
+        <>
+            <h1>Hello World App !</h1>
+            <h2>Meu primeiro Comando no React</h2>
+        </>
+    )
+}
+
+export default Hello;
