4bd4d141d0c39bc400fc6391fbf52efc5553b8e9
Antonio Felix de Oliveira Neto <antoniofelixxx7@gmail.com>
2024-10-19 16:22:14 -0300

    .

---
+++ estudos Front-End/JavaScript/aulas/aula11/ex009.js
@@ -0,0 +1,10 @@
+var país = 'Brasil'
+var sexo = 'F'
+
+
+console.log(`Vivendo em ${país}`)
+if (país != 'Brasil' && sexo == 'F') {
+    console.log('Você é Estrangeira')
+}else {
+    console.log('Você é Brasileira')
+}
---
+++ estudos Front-End/JavaScript/aulas/aula13/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1
+var f = 6
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= f ) //Teste lógico no final.
+
+
+/* var c = 1
+    var f = 6
+while (c <= f){
+    console.log('Tudo bem?')
+    c++
+} Teste lógico no início.
+*/
