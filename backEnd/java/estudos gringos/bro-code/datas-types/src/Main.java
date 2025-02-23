e34df4ef76035e00dfbc9133378b2616c9f072ad
Antonio Felix de Oliveira Neto <antoniofelixxx7@gmail.com>
2025-02-23 00:30:47 -0300

    estudos

--- backEnd/java/estudos gringos/bro-code/datas-types/src/Main.java
+++ backEnd/java/estudos gringos/bro-code/datas-types/src/Main.java
@@ -1,2 +1,63 @@
+/*
+       * Primitivo                  * Reference (ponteiro)
+        Direto na memória             Endereço de memória
+          1. int                       1. string
+          2. double                    2. array
+          3. char                      3. object
+          4. boolean
+
+       2 Steeps to creating a variable
+
+       * 1. declaration
+            -> int age = 22;
+            -> sout(age)
+
+       * 2. assignment
+ */
+
 public class Main {
+
+    public static void main (String[] args) {
+
+        // Parte 2 -> Primitive
+
+        // int
+        int age = 22;
+
+        // double
+        double num = 22.5;
+
+        // char -> usa ''
+        char sex = 'M';
+
+        // boolean
+        boolean hasChild = false;
+        boolean isOnline = false;
+        boolean hasMoney = true;
+
+        // output
+        System.out.println("int -> " + age);
+        System.out.println("double -> " + num);
+        System.out.println("char -> " + sex);
+        System.out.println("I Has child? -> " + hasChild);
+
+
+        // condição if/else
+        if(isOnline) {
+            System.out.println("The user's on");
+        } else {
+            System.out.println("The user's off");
+        }
+
+        // ternario
+        System.out.println((hasMoney) ? "I have Money" : "I dont have money");
+
+
+        // Part 2 -> reference
+
+        // string
+        String nome = "Antonio Felix";
+
+        System.out.println("String -> " + nome);
+    }
 }
--- frontEnd/gringos/estudos gringos/travessyMedia/JS2/01-variables-data-types/09-strings/script.js
+++ frontEnd/gringos/estudos gringos/travessyMedia/JS2/01-variables-data-types/09-strings/script.js
@@ -1,18 +1,17 @@
 let x;
 
-const s = "Hello, galerinha de cowboy."
+const s = "Hello, galerinha de cowboy.";
 
 x = s.length;
 
-x = s.indexOf('H');
+x = s.indexOf("H");
 
 x = s.slice(-20);
 
-x = s.replace('Hello', 'buhh');
+x = s.replace("Hello", "buhh");
 
-x = s.substring(0 , 2) // (1)
+x = s.substring(0, 2); // (1)
 
-x = s.split(' '); // Dividir a String.
+// x = s.split(' '); // Dividir a String.
 
 console.log(x);
-
